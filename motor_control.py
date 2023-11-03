52957fbd945745d31f7345f4a8522d27cab2d094
gmbord <bordelon@mit.edu>
2023-11-03 19:25:24 -0400

    a

--- motor_control.py
+++ motor_control.py
@@ -6,16 +6,16 @@ BRUSH_L = 12
 BRUSH_R = 13
 
 pi = pigpio.pi()
-pi.set_duty_cycle(DRIVE, 0)
-pi.set_duty_cycle(BRUSH_L, 0)
-pi.set_duty_cycle(BRUSH_R, 0)
+pi.set_PWM_dutycycle(DRIVE, 0)
+pi.set_PWM_dutycycle(BRUSH_L, 0)
+pi.set_PWM_dutycycle(BRUSH_R, 0)
 
     
 def set_drive_speed(speed):
-    pi.set_duty_cycle(DRIVE, speed)
+    pi.set_PWM_dutycycle(DRIVE, speed)
     print("DRIVE SPEED SET TO: " + speed)
     
 def set_brush_speed(speed):
-    pi.set_duty_cycle(BRUSH_L, speed)
-    pi.set_duty_cycle(BRUSH_R, speed)
+    pi.set_PWM_dutycycle(BRUSH_L, speed)
+    pi.set_PWM_dutycycle(BRUSH_R, speed)
     print("BRUSH SPEED SET TO: " + speed)
