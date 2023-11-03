6c3d551d1ad9b784f96adfb00fb5ee2564801b2b
gmbord <bordelon@mit.edu>
2023-11-03 19:14:21 -0400

    motor control

--- motor_control.py
+++ motor_control.py
@@ -1,5 +1,21 @@
 import time
-import RPi.GPIO as GPIO
+import pigpio
 
-def test_motor():
-    print("MOTOR 1 SIGNAL: 20%")
+DRIVE = 18
+BRUSH_L = 12
+BRUSH_R = 13
+
+pi = pigpio.redpi()
+pi.set_duty_cycle(DRIVE, 0)
+pi.set_duty_cycle(BRUSH_L, 0)
+pi.set_duty_cycle(BRUSH_R, 0)
+
+    
+def set_drive_speed(speed):
+    pi.set_duty_cycle(DRIVE, speed)
+    print("DRIVE SPEED SET TO: " + speed)
+    
+def set_brush_speed(speed):
+    pi.set_duty_cycle(BRUSH_L, speed)
+    pi.set_duty_cycle(BRUSH_R, speed)
+    print("BRUSH SPEED SET TO: " + speed)
