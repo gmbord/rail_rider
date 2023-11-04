452af372de1f96e5ff180dc7a05552665a4e6823
gmbord <bordelon@mit.edu>
2023-11-04 18:36:04 -0400

    remove files

--- rc.py
+++
@@ -1,9 +0,0 @@
-import read_sbus_from_GPIO
-
-SBUS_PIN = 4 #pin where sbus wire is plugged in, BCM numbering
-
-reader = read_sbus_from_GPIO.SbusReader(SBUS_PIN)
-reader.begin_listen()
-
-def test_func():
-    reader.display_latest_packet()
