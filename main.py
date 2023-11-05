4d016f51a100efd11928ca1acef5c87203bfa649
gmbord <bordelon@mit.edu>
2023-11-04 20:56:17 -0400

    skelly

--- main.py
+++ main.py
@@ -7,13 +7,98 @@ from contactor_control import *
 from elight_control import *
 
 
-while True:
-    test_func()
-    set_drive_speed(0.2)
-    set_brush_speed(0.2)
-    activate_brush_power()
-    activate_drive_power()
+
+
+Drive_Armed = False
+Brush_Armed = False
+
+drive_armed_chanel = 6
+brush_armed_chanel = 7
+drive_trotle_chanel = 1
+brush_trotle_chanel = 3
+elight_chanel = 8
+linear_chanel = 10
+
+initialize_robot()
+main_control_loop()
+
+
+def update_armed():
+    sig = read_sbus_chanel(drive_armed_chanel)
+    sig2 = read_sbus_chanel(brush_armed_chanel)
+    if sig > 1500:
+        Drive_Armed = True
+    else:
+        Drive_Armed = False
+    if sig2 > 1500:
+        Brush_Armed = True
+    else:
+        Brush_Armed = False
+        
+def update_drive():
+    sig = read_sbus_chanel(drive_trotle_chanel)
+    if sig >= 1000 and sig <= 2000:
+        power = (sig - 1000) / 1000 #max is full
+        if power > 1:
+            power = 1
+        if power < 0.02:
+            power = 0
+        set_drive_speed(power)
+
+def update_brush():
+    sig = read_sbus_chanel(brush_trotle_chanel)
+    if sig >= 1000 and sig <= 2000:
+        power = (sig - 1000) / 2000 #max is half power
+        if power > 0.5:
+            power = 0.5
+        if power < 0.02:
+            power = 0
+        set_drive_speed(power)
+    
+def update_elight():
+    sig = read_sbus_chanel(elight_chanel)
+    if sig > 1500:
+        activate_emergency_light()
+    else:
+        deactivate_emergency_light()
+        
+def update_linear():
+    sig = read_sbus_chanel(linear_chanel)
+    if sig < 1200 and sig >= 1000:
+        lower_linear()
+    elif sig > 1200 and sig <= 1800:
+        stop_linear()
+    elif sig > 1800 and sig <= 2000:
+        raise_linear()
+    else:
+        stop_linear()
+    
+def initialize_robot():
+    deactivate_brush_power()
+    deactivate_drive_power()
     activate_emergency_light()
-    lower_linear()
-    time.sleep(1)
+    stop_linear()
+    set_drive_speed(0)
+    set_brush_speed(0)
+
+def main_control_loop():
+    while True:
+        update_armed()
+        if Drive_Armed:
+            activate_drive_power()
+            update_drive()
+        else:
+            deactivate_drive_power()
+            set_drive_speed(0)
+        if Brush_Armed:
+            activate_brush_power()
+            update_brush()
+        else:
+            deactivate_brush_power()
+            set_brush_speed(0)
+        
+        update_elight()
+        update_linear()
+        display_sbus()
+        time.sleep(1)
 
--- rc.py
+++ rc.py
@@ -5,5 +5,8 @@ SBUS_PIN = 4 #pin where sbus wire is plugged in, BCM numbering
 reader = read_sbus_from_GPIO.SbusReader(SBUS_PIN)
 reader.begin_listen()
 
-def test_func():
+def display_sbus():
     reader.display_latest_packet()
+    
+def read_sbus_chanel(chanel):
+    return reader.translate_latest_packet()[chanel-1]
