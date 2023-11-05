a13e915e68606f37cc10b4a70a0a5d62c61ac112
gmbord <bordelon@mit.edu>
2023-11-05 13:51:18 -0500

    :

--- src/rc.py
+++ src/rc.py
@@ -11,5 +11,5 @@ def display_sbus():
 def read_sbus_chanel(chanel):
     return reader.translate_latest_packet()[chanel-1]
 
-def sbus_connected()
+def sbus_connected():
     return reader.is_connected()
