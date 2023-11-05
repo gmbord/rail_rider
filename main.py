d566fad0a76b378dc7e332ef4e7654d674361489
gmbord <bordelon@mit.edu>
2023-11-04 20:06:39 -0400

    import

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import time
 import RPi.GPIO as GPIO
-from src import *
-# from src/rc import *
-# from src/motor_control import *
-# from src/linear_control import *
+from src/rc import *
+from src/motor_control import *
+from src/linear_control import *
+from src/contactor_control import *
+from src/elight_control import *
 
 
 while True:
     test_func()
     set_drive_speed(0.2)
     set_brush_speed(0.2)
-    time.sleep(1)
+    
 
-
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(13, GPIO.OUT)
-
-# p = GPIO.PWM(13, 50)
-# p.start(0)
-# time.sleep(10)
-
-# p.ChangeDutyCycle(3)
