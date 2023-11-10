1b25240fba1bc683c6a324c4fe10671f5e6a2585
gmbord <bordelon@mit.edu>
2023-11-10 00:18:03 -0500

    pwm stuff

--- src/motor_control.py
+++ src/motor_control.py
@@ -1,7 +1,7 @@
 import time
 import pigpio
 
-#Max 3.3 V -> 1
+#Max 3.5 V -> 1
 #Min 0.875 V -> 0.265
 
 DRIVE = 18
@@ -12,16 +12,30 @@ pi = pigpio.pi()
 pi.set_mode(DRIVE, pigpio.OUTPUT)
 pi.set_mode(BRUSH_L, pigpio.OUTPUT)
 pi.set_mode(BRUSH_R, pigpio.OUTPUT)
+pi.set_PWM_frequency(DRIVE, 40)
+pi.set_PWM_frequency(BRUSH_L, 40)
+pi.set_PWM_frequency(BRUSH_R, 40)
+print(pi.get_PWM_frequency(DRIVE))
 pi.set_PWM_dutycycle(DRIVE, 0)
 pi.set_PWM_dutycycle(BRUSH_L, 0)
 pi.set_PWM_dutycycle(BRUSH_R, 0)
 
     
 def set_drive_speed(speed):
+    speed = 255*speed
+    if speed > 255:
+        speed = 255
+    if speed < 0:
+        speed = 0
     pi.set_PWM_dutycycle(DRIVE, speed)
     print("DRIVE SPEED SET TO: ", (speed))
     
 def set_brush_speed(speed):
+    speed = 255*speed
+    if speed > 255:
+        speed = 255
+    if speed < 0:
+        speed = 0
     pi.set_PWM_dutycycle(BRUSH_L, speed)
     pi.set_PWM_dutycycle(BRUSH_R, speed)
     print("BRUSH SPEED SET TO: ", (speed))
