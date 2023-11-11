2f5e35535288348c2ba7874cb7a2339f524f8f67
gmbord <bordelon@mit.edu>
2023-11-11 15:24:39 -0500

    s

--- serial_test.py
+++ serial_test.py
@@ -19,7 +19,7 @@ Ubuntu) and the same Arduino.
 import serial
 import time
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 # Reset the Arduino's line. This is key to getting the write to work.
 # Without it, the first few writes don't work.
@@ -28,34 +28,34 @@ ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 # This trick was learned from:
 # https://github.com/miguelasd688/4-legged-robot-model
 
-ser.setDTR(False)
-time.sleep(1)
-ser.flushInput()
-ser.setDTR(True)
-time.sleep(2)
+
 
 while True:
 
 	print('Telling the Arduino to start blinking...')
-	ser.write(b'1')
+	ser.write(b'200')
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
+	time.sleep(0.5)
+ 
 
-	# read to get the acknowledgement from the Arduino
-	while True:
-		ack = ser.read()
-		if ack == b'A':
-			break
-	print('Arduino sent back %s' % ack)
+	# # read to get the acknowledgement from the Arduino
+	# while True:
+	# 	ack = ser.read()
+	# 	if ack == b'A':
+	# 		break
+	# print('Arduino sent back %s' % ack)
 
-	time.sleep(2)
+	# time.sleep(2)
 
-	print('Telling the Arduino to stop blinking...')
-	ser.write(b'0')
+	# print('Telling the Arduino to stop blinking...')
+	# ser.write(b'0')
 
-	# read to get the acknowledgement from the Arduino
-	while True:
-		ack = ser.read()
-		if ack == b'A':
-			break
-	print('Arduino sent back %s' % ack)
+	# # read to get the acknowledgement from the Arduino
+	# while True:
+	# 	ack = ser.read()
+	# 	if ack == b'A':
+	# 		break
+	# print('Arduino sent back %s' % ack)
 
-	time.sleep(2)
+	# time.sleep(2)
