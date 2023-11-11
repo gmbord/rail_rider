90b559892338289b640ac21f70a62334ce6fe38e
gmbord <bordelon@mit.edu>
2023-11-11 15:35:51 -0500

    d

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def update_drive():
         power = 1
     if power < 0.02:
         power = 0
-    # power = 0.3
+    power = 0.5
     output = str(int(255*power))+ '\n'
     out = bytes(output, 'utf-8')
     ser.write(out)
