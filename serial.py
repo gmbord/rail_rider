2e5baf7735b05d21df7863bdbb2e5e182fc72129
gmbord <bordelon@mit.edu>
2023-11-10 23:40:12 -0500

    s

--- serial.py
+++ serial.py
@@ -1,3 +1,21 @@
+
+"""
+serial_usb_simple.py  Demo that communicates over USB using serial I/O
+from a Raspberry Pi to an Arduino.
+
+To show that it work, this writes a '1' to the Arduino which then 
+blinks the builtin LED on and off. The Arduino also sends back an 'A'
+to acknowledge that it got the message. This does a read() to get
+the 'A', demonstrating that reading also works. Two seconds later,
+this writes a '0' to the Arduino which then stops the blinking.
+The Arduino again sends back an 'A' to acknowledge that it got the
+message and this reads the 'A'.
+
+This was tested between a Raspberry Pi 3B (running Raspbian) and
+an Arduino Mega 2560 and also between an NVIDIA Jetson TX1 (running 
+Ubuntu) and the same Arduino.
+"""
+
 import serial
 import time
 
