796aa9e0b3f724d7d3c3350cb66900ae4d9eaec0
gmbord <bordelon@mit.edu>
2023-11-23 15:39:33 -0600

    h

--- jetson_cv/obstacle_detect.py
+++ jetson_cv/obstacle_detect.py
@@ -3,7 +3,7 @@ import pyrealsense2
 from realsense_depth import *
 
 
-# dc = DepthCamera()
+dc = DepthCamera()
 # ret, depth_frame, color_frame = dc.get_frame()
 # cv2.imhow("Color frame", color_frame)
 # cv2.waitKey()
