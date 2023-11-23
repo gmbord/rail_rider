6dafb193e9b326db4383694fd8dfae39b7b349a0
gmbord <bordelon@mit.edu>
2023-11-23 13:58:40 -0600

    g

--- jetson_cv/obstacle_detect.py
+++ jetson_cv/obstacle_detect.py
@@ -1,1 +1,16 @@
-print("beggining obstacle detection")
+import numpy
+import cv2
+
+fx = 942.8          # lense focal length
+baseline = 54.8     # distance in mm between the two cameras
+disparities = 128   # num of disparities to consider
+block = 31          # block size to match
+units = 0.001       # depth units
+
+sbm = cv2.StereoBM_create(numDisparities=disparities,
+                          blockSize=block)
+
+disparity = sbm.compute(left, right)
+
+depth = np.zeros(shape=left.shape).astype(float)
+depth[disparity > 0] = (fx * baseline) / (units * disparity[disparity > 0])
