df7be257409669dcb184f5b0da4d280561a952a5
gmbord <bordelon@mit.edu>
2023-11-23 15:12:52 -0600

    s

--- jetson_cv/obstacle_detect.py
+++ jetson_cv/obstacle_detect.py
@@ -1,17 +1,9 @@
-import numpy
 import cv2
 import pyrealsense2
+from realsense_depth import *
 
-fx = 942.8          # lense focal length
-baseline = 54.8     # distance in mm between the two cameras
-disparities = 128   # num of disparities to consider
-block = 31          # block size to match
-units = 0.001       # depth units
 
-# sbm = cv2.StereoBM_create(numDisparities=disparities,
-#                           blockSize=block)
-
-# disparity = sbm.compute(left, right)
-
-# depth = np.zeros(shape=left.shape).astype(float)
-# depth[disparity > 0] = (fx * baseline) / (units * disparity[disparity > 0])
+dc = DepthCamera()
+ret, depth_frame, color_frame = dc.get_frame()
+cv2.imhow("Color frame", color_frame)
+cv2.waitKey()
