764ab9dfeae5b376f608547f41333c6b74e803f4
gmbord <bordelon@mit.edu>
2023-11-23 15:39:08 -0600

    f

--- jetson_cv/obstacle_detect.py
+++ jetson_cv/obstacle_detect.py
@@ -3,7 +3,8 @@ import pyrealsense2
 from realsense_depth import *
 
 
-dc = DepthCamera()
-ret, depth_frame, color_frame = dc.get_frame()
-cv2.imhow("Color frame", color_frame)
-cv2.waitKey()
+# dc = DepthCamera()
+# ret, depth_frame, color_frame = dc.get_frame()
+# cv2.imhow("Color frame", color_frame)
+# cv2.waitKey()
+print("the n word")
