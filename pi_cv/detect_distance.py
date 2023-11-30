5cae21837ba051ea626a94197ebbb94974181dfe
gmbord <bordelon@mit.edu>
2023-11-30 12:28:01 -0500

    d

--- pi_cv/detect_distance.py
+++ pi_cv/detect_distance.py
@@ -45,24 +45,24 @@ def get_frame(pipeline):
 
 # dc1 = DepthCamera()
 # dc2 = DepthCamera()
-# pyrealsense2.hole_filling_filter(2)
+pyrealsense2.hole_filling_filter(2)
 
 
-# while True:
-#     ret, depth_frame = get_frame(pipeline_1)
-#     # ret2, depth_frame2, color_frame2 = get_frame(pipeline_2)
-#     # Show distance for a specific point
-#     # cv2.circle(color_frame, point, 4, (0, 0, 255))
-#     distance = depth_frame[point[1], point[0]]
-#     # distance2 = depth_frame2[point[1], point[0]]
-#     # if distance > 0:
-#     print("DISTANCE 1 ", distance)
-#     # print("DISTANCE 2 ", distance2)
-#     if distance < scary_range and distance > 0:
-#         print("$$$$$$$$ KILLLLLL  1   $$$$$$$$$$$$$$$")
-#     # if distance2 < scary_range and distance > 0:
-#     #     print("$$$$$$$$ KILLLLLL  2   $$$$$$$$$$$$$$$")
-#     time.sleep(0.02)
+while True:
+    ret, depth_frame = get_frame(pipeline_1)
+    # ret2, depth_frame2, color_frame2 = get_frame(pipeline_2)
+    # Show distance for a specific point
+    # cv2.circle(color_frame, point, 4, (0, 0, 255))
+    distance = depth_frame[point[1], point[0]]
+    # distance2 = depth_frame2[point[1], point[0]]
+    # if distance > 0:
+    print("DISTANCE 1 ", distance)
+    # print("DISTANCE 2 ", distance2)
+    if distance < scary_range and distance > 0:
+        print("$$$$$$$$ KILLLLLL  1   $$$$$$$$$$$$$$$")
+    # if distance2 < scary_range and distance > 0:
+    #     print("$$$$$$$$ KILLLLLL  2   $$$$$$$$$$$$$$$")
+    time.sleep(0.02)
 
 #     # cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
