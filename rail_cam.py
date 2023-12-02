65d61f4deba4cf01da2e481d13a782b8ea15a7bc
Maxwell Wang <max.z.wang@gmail.com>
2023-12-02 16:48:31 -0500

    Try live capture and average pixel calculation

--- rail_cam.py
+++ rail_cam.py
@@ -1,21 +1,38 @@
 import cv2
+import time
+import numpy as np
 
-# Initialize the USB camera (assuming it's connected and recognized)
 camera = cv2.VideoCapture(0)  # Use 0 for the default camera
 
 if not camera.isOpened():
     print("Cannot open camera")
     exit()
 
-# Capture a frame
-ret, frame = camera.read()
+while True:
+    ret, frame = camera.read()
 
-if not ret:
-    print("Failed to capture frame")
-    exit()
+    if not ret:
+        print("Failed to capture frame")
+        break
 
-# Save the captured frame as an image
-cv2.imwrite("test.jpg", frame)
+    # Calculate the average pixel value
+    average_pixel_value = np.mean(frame)
+    print(f"Average pixel value: {average_pixel_value}")
 
-# Release the camera
+    # Assign the captured frame to the variable L_front
+    L_front = frame
+
+    # Save the captured frame as 'test.jpg' (optional)
+    cv2.imwrite("test.jpg", frame)
+
+    # Display the captured frame (optional)
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+    # Wait for 1 second before capturing the next frame
+    time.sleep(1)
+
+# Release the camera and close the OpenCV windows
 camera.release()
+cv2.destroyAllWindows()
