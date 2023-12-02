9d1ff9d89f92132e21fb6fbabe4b3f966b8c4644
Maxwell Wang <max.z.wang@gmail.com>
2023-12-02 16:12:02 -0500

    test picamera2

--- rail_cam.py
+++ rail_cam.py
@@ -1,9 +1,10 @@
-import picamera2
-from time import sleep
+from picamera2 import Picamera2, Preview
+import time
 
-with picamera2.PiCamera() as camera:
-    camera.resolution = (1280, 720)  # Set the resolution
-    camera.start_preview()
-    sleep(5)  # Allow time for the camera to adjust to light
-    camera.capture("image.jpg")  # Capture an image and save it as image.jpg
-    camera.stop_preview()
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.DRM)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
