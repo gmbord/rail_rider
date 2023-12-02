0e563c619a533fc2d5103ab70903d8f127b3eff0
Maxwell Wang <max.z.wang@gmail.com>
2023-12-02 16:02:16 -0500

    test picamera

--- rail_cam.py
+++ rail_cam.py
@@ -1,3 +1,9 @@
-# rail camera code
+import picamera
+from time import sleep
 
-print("who's joe")
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)  # Set the resolution
+    camera.start_preview()
+    sleep(5)  # Allow time for the camera to adjust to light
+    camera.capture("image.jpg")  # Capture an image and save it as image.jpg
+    camera.stop_preview()
