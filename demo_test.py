68683258d731c7611ed0e0a1dfefcd33328bf72b
gmbord <bordelon@mit.edu>
2023-12-10 03:46:22 -0500

    1

--- demo_test.py
+++ demo_test.py
@@ -438,6 +438,7 @@ def reset_demo():
     
 def kill_demo():
     globals().update(demo_running = False)
+    print("$$$$$$$$$$$$$$$ KILL DEMO $$$$$$$$$$$$$$$$$$$$$")
     print("DEMO IS RUNNING ", demo_running)
     initialize_robot()
     # reset_demo()
